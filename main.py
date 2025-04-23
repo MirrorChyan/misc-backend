b2df80b614a5f05d671e3a1c55acbcaddf5fe745
MistEO <mistereo@hotmail.com>
2025-04-23 14:39:05 +0800

    fix: icp router

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from src.anno import router as anno_router
 from src.plan import router as plan_router
 from src.health_check import router as health_check_router
 from src.project import router as project_router
+from src.ICP import router as icp_router
 
 app = FastAPI()
 
@@ -14,4 +15,5 @@ app.include_router(anno_router)
 app.include_router(plan_router)
 app.include_router(health_check_router)
 app.include_router(project_router)
+app.include_router(icp_router)
 
