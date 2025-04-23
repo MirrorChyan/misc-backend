391cf5bb7d66e4bd6ae75a4f5c30ffb7f664bef8
MistEO <mistereo@hotmail.com>
2025-04-23 14:53:16 +0800

    fix: error param

--- src/ICP/__init__.py
+++ src/ICP/__init__.py
@@ -8,7 +8,7 @@ router = APIRouter()
 
 
 @router.get("/icp")
-async def query_icp(lang: str):
+async def query_icp():
     return {
         "icp_beian": settings.icp_beian,
         "icp_url": settings.icp_url,
