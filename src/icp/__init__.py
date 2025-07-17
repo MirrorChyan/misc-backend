8af23da0946b606079fd4ddf8b086067b9bf3892
Aliothmoon <aliothmoon@outlook.com>
2025-07-17 16:12:22 +0800

    fix: ignore case false

---
+++ src/icp/__init__.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, Request
+from loguru import logger
+
+from src.database import ICP
+
+router = APIRouter()
+
+AllICP = ICP.select()
+
+
+@router.get("/icp")
+async def query_icp(domain: str):
+    icp = next(
+        (icp for icp in AllICP if icp.domain in domain),
+        None,
+    )
+
+    if icp is None:
+        logger.error(f"domain not found: {domain}")
+        return {
+            "domain": domain,
+            "icp_beian": "",
+            "icp_url": "",
+            "icp_entity": "",
+        }
+
+    return {
+        "domain": domain,
+        "icp_beian": icp.beian,
+        "icp_url": icp.url,
+        "icp_entity": icp.entity,
+    }
