4442e66364610b4dd5cb4bbe7480ac270f44f770
MistEO <mistereo@hotmail.com>
2025-07-17 13:57:03 +0800

    feat: add lock for data cache

--- src/contact_us/__init__.py
+++ src/contact_us/__init__.py
@@ -8,24 +8,23 @@ router = APIRouter()
 
 CacheExpiration = 600  # 秒
 cache = None
+cache_lock = asyncio.Lock()
 
-def get_cache():
-    now = time()
-    
+
+async def get_cache():
     global cache
-    if not cache or (now - cache[1] > CacheExpiration):
-        cache = (list(ContactUs.select()), now)
 
+    async with cache_lock:
+        now = time()
+        if not cache or (now - cache[1] > CacheExpiration):
+            cache = (list(ContactUs.select()), now)
     return cache[0]
+
 
 @router.get("/contact_us")
 async def contact_us():
     data = {}
-    for c in get_cache():
+    for c in await get_cache():
         data[c.channel] = c.detail
 
-    return {
-        "ec": 200,
-        "code": 0,
-        "data": data
-    }
+    return {"ec": 200, "code": 0, "data": data}
